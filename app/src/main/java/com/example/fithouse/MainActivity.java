423a25d8ea8a42c1e41c0f4cbdd08d229ea798de
sebasmazo <sebastianmazovelez@gmail.com>
2022-04-17 19:19:26 -0500

    v 0.3
    Second activity with navigation bar

--- app/src/main/java/com/example/fithouse/MainActivity.java
+++ app/src/main/java/com/example/fithouse/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.fithouse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Handler manejador = new Handler(Looper.getMainLooper());
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int tiempoTranscurrir = 5000;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manejador.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+            }
+        },tiempoTranscurrir);
     }
+
+
 }
